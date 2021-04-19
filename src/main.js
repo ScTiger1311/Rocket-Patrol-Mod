3055dafd51149bb5f5b2e10524fe20c8ca9ddd3e
Ethan Rafael <winsoftheepic@gmail.com>
2021-04-19 16:02:40 -0700

    Added a comment for grading

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ For graders--
 Mods:
 Game Physics not tied to frame rate. Used a deltaTime to accomplish this. Approved by Nathan in discord, (10 points) 1 hour
 Timer is shown (10 points). 1.5 hours but only because I was foolishly stubborn and tried to do it a dumb way first
-Redesign the game's artwork, ui, and sound to change it's aesthetic (60 points), 6 hours. 
+Redesign the game's artwork, ui, and sound to change it's aesthetic (60 points), 5 hours. 
 Add your own copyright free music (5 points) 1 hour.
-Increase speed after half the time has elapsed (5 points). 20 minutes
+Increase speed after half the time has elapsed (5 points). 30 minutes
+Animated spaceship sprite (10 points) 1 hour.
 
+Total: 100 Points, 10 hours
 */
 
 let config =
